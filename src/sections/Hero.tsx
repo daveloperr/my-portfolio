b602af8835e3c2bb4de02de963c4f87ff2d50aab
davelopez. <mrdavelopez28@gmail.com>
2026-05-19 21:43:35 +0800

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,32 +1,36 @@
+import NavBar from "./../components/NavBar";
+
+
+
 export default function Hero() {
     return (
-        <div className="h-screen flex flex-col px-5 overflow-hidden">
+      <div className="h-screen px-6 flex flex-col justify-between pb-12 ">
+            
+    <NavBar />
 
-            <div className="flex justify-between items-start">
-                <h1 className="py-5 lg:text-5xl tracking-tighter">
-                    Building
-                    <span className="font-serif italic font-medium tracking-normal ml-2">
-                        Digital Experiences
-                    </span>
-                    <br />through code.
-                </h1>
-
-                <img
-                    src="/me.png"
-                    alt="Dave Lopez Picture"
-                    className="w-[350px] h-[350px] object-cover"
-                />
-            </div>
-
-            <div className="mt-auto pb-12 flex flex-col ">
-                <h2 className="font-sans uppercase text-2xl text-gray-400 -mb-2 tracking-tighter text-left">
-                    Becoming
-                </h2>
-
-                <h3 className="uppercase text-[200px] font-bold leading-[0.85] tracking-tighter text-left -ml-[10px]">
+    <div className="w-full pt-4">
+                <h3 className="uppercase text-[11.5vw] font-extrabold leading-[0.85] tracking-tighter text-left -ml-[10px]">
                     DaveLopez
                 </h3>
+
+    </div>
+
+            <div className="grid grid-cols-12 gap-6 items-end">
+                <div className="col-span-7">
+                    <h1 className="font-sans lg:text-2xl tracking-tighter max-w-5xl balance">
+                        I build reliable, well-engineered web applications that prioritize clean code and exceptional user experience. My focus is on delivering structured, production-ready solutions that look professional and perform flawlessly.
+                    </h1>
+                </div>
+
+                <div className="col-span-5 flex justify-end">
+                    <img
+                        src="/me.png"
+                        alt="Picture"
+                        className="w-[350px] h-[350px] object-cover"
+                    />
+                </div>
             </div>
+
         </div>
     );
 }
