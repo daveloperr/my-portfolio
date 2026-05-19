826f577b3b7981a02eff5b284ec72a9cc82ed46a
davelopez. <mrdavelopez28@gmail.com>
2026-05-19 21:44:55 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
-import Image from "next/image";
 import NavBar from "../components/NavBar";
 import Hero from "../sections/Hero";
 import ProjectGrid from "../components/projects/ProjectGrid";
+import { TechStack } from "../sections/TechStack";
+import Quote from "../sections/Quote";
+
 
 export default function Home() {
   return (
-    <div>
-      <NavBar />
-      <Hero/>
-      <ProjectGrid/>
-    
+    <div className="relative">
+
+      {/* <NavBar /> */}
+
+      {/* GRID OVERLAY */}
+      <div className="pointer-events-none fixed inset-0 z-[99999] px-6">
+        <div className="grid h-full w-full grid-cols-12 gap-6">
+          {Array.from({ length: 12 }).map((_, i) => (
+            <div key={i} className="border border-red-500/80 h-full" />
+          ))}
+        </div>
+      </div>
+
+      <Hero />
+      <ProjectGrid />
+      <Quote />
+            <TechStack />
+
     </div>
   );
 }
