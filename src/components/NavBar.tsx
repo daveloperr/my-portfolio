54226262a47fce4a04e833375db597e1627b6148
dave <mrdavelopez28@gmail.com>
2026-05-21 23:37:27 +0800

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,22 @@
-
-
-
 export default function NavBar() {
-    return (
-        <nav className="flex items-center gap-x-8 py-4 text-xs font-bold tracking-wider text-neutral-500">
-            <a href="#" className="font-bold text-black uppercase mr-auto">
-                DaveLopez
-            </a>
-            <a href="#about" className="hover:text-black transition-colors uppercase">
-                About
-            </a>
-            <a href="#works" className="hover:text-black transition-colors uppercase">
-                Works
-            </a>
-            <a href="#contact" className="hover:text-black transition-colors uppercase">
-                Contact
-            </a>
-            <span className="text-neutral-400 select-none">|</span>
-            <span className="uppercase text-neutral-400">
-                Manila, PH
-            </span>
-        </nav>
+  return (
+    <nav className="flex items-center py-4 text-xs font-bold tracking-wider text-neutral-500">
+      
+      <a href="#" className="font-bold text-black uppercase">
+        VAUGHNLOPEZ
+      </a>
 
-    )
+      <div className="flex justify-evenly flex-1">
+        <a href="#about" className="hover:text-black uppercase">About</a>
+        <a href="#about" className="hover:text-black uppercase">Skills</a>
+        <a href="#works" className="hover:text-black uppercase">Works</a>
+        <a href="#contact" className="hover:text-black uppercase">Contact</a>
+      </div>
 
+      <a href="#contact" className="hover:text-black uppercase">
+        Hire Me
+      </a>
+
+    </nav>
+  );
 }
