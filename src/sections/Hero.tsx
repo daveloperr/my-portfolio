94e976dc604ddb36f81d68300e043c8382ca2b38
dave <mrdavelopez28@gmail.com>
2026-05-21 23:37:47 +0800

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,36 +1,136 @@
+"use client";
+
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import NavBar from "./../components/NavBar";
 
-
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Hero() {
+    const sectionRef = useRef<HTMLElement>(null);
+    const imgWrapperRef = useRef<HTMLDivElement>(null);
+    const titleRef = useRef<HTMLDivElement>(null!);
+    const navbarRef = useRef<HTMLDivElement>(null!);
+    const paragraphRef1 = useRef<HTMLDivElement>(null);
+    const paragraphRef2 = useRef<HTMLDivElement>(null);
+
+
+
+    useLayoutEffect(() => {
+        const ctx = gsap.context(() => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: sectionRef.current,
+                    start: "top top",
+                    end: "+=140%",
+                    scrub: 0.3,
+                    pin: true,
+                    anticipatePin: 1,
+                    invalidateOnRefresh: true,
+                },
+            });
+
+            const texts = [
+                titleRef.current,
+                paragraphRef1.current,
+                paragraphRef2.current,
+            ].filter(Boolean);
+
+
+
+            tl.to(texts, {
+                clipPath: "inset(0% 0% 100% 0%)",
+                ease: "none",
+            }, 0);
+
+            tl.to(texts, {
+                clipPath: "inset(0% 0% 100% 0%)",
+                ease: "none",
+            }, 1);
+
+            tl.to(navbarRef.current, {
+                y: () => -titleRef.current.offsetHeight,
+                ease: "power3.inOut",
+            }, 0);
+
+
+            tl.to(imgWrapperRef.current, {
+                scale: 2,
+                y: () => -window.innerHeight / 2 + 250,
+                ease: "power2.out",
+            }, 0);
+
+
+
+
+        }, sectionRef);
+
+        return () => ctx.revert();
+    }, []);
+
     return (
-      <div className="h-screen px-6 flex flex-col justify-between pb-12 ">
-            
-    <NavBar />
+        <>
+            <section
+                ref={sectionRef}
+                className="relative h-screen px-6  overflow-hidden flex flex-col"
+            >
+                <div className="relative w-full">
 
-    <div className="w-full pt-4">
-                <h3 className="uppercase text-[11.5vw] font-extrabold leading-[0.85] tracking-tighter text-left -ml-[10px]">
-                    DaveLopez
-                </h3>
+                    <div
+                        ref={titleRef}
+                        className="w-full flex overflow-hidden"
+                    >
+                        <h3 className="uppercase text-[13.6vw] font-bold leading-[0.85] tracking-[-0.05em]">
+                            VaughnLopez
+                        </h3>
+                    </div>
 
-    </div>
+                    <div
+                        ref={navbarRef}
+                        className="absolute left-0 w-full z-20"
+                        style={{ top: "100%" }}   
+                    >
+                        <NavBar />
+                    </div>
 
-            <div className="grid grid-cols-12 gap-6 items-end">
-                <div className="col-span-7">
-                    <h1 className="font-sans lg:text-2xl tracking-tighter max-w-5xl balance">
-                        I build reliable, well-engineered web applications that prioritize clean code and exceptional user experience. My focus is on delivering structured, production-ready solutions that look professional and perform flawlessly.
-                    </h1>
                 </div>
 
-                <div className="col-span-5 flex justify-end">
-                    <img
-                        src="/me.png"
-                        alt="Picture"
-                        className="w-[350px] h-[350px] object-cover"
-                    />
-                </div>
-            </div>
+                <div className="absolute inset-0 flex items-end justify-center pb-16">
 
-        </div>
+
+                    <div className="flex flex-col items-center">
+
+                        <div
+                            ref={imgWrapperRef}
+                            className="z-20 overflow-hidden rounded-md origin-center"
+                            style={{ width: "200px", height: "200px" }}
+                        >
+                            <img
+                                src="/me.png"
+                                alt="Vaughn Lopez"
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+
+                        <div className="mt-3 z-10 overflow-hidden">
+                            <div ref={paragraphRef1}>
+                                <h2 className="uppercase lg:text-5xl tracking-tighter leading-[0.85] text-center font-extrabold">
+                                    FULL-STACK DEVELOPER / UI & UX DESIGNER CRAFTING
+                                </h2>
+                            </div>
+                            <div ref={paragraphRef2}>
+                                <h2 className="uppercase lg:text-5xl tracking-tighter leading-[0.85] text-center font-extrabold">
+                                    WITH PRECISION AND INTENT.
+                                </h2>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </section>
+
+        </>
     );
 }
