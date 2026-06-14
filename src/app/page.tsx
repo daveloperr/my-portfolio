9111d0eb1d8f98d6ac7bf5c0a2f2f1bbb82688bb
dave <mrdavelopez28@gmail.com>
2026-06-14 22:47:08 +0800

    updated the global page w/ navbar and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,48 @@
-import NavBar from "../components/NavBar";
-import Hero from "../sections/Hero";
-import ProjectGrid from "../components/projects/ProjectGrid";
-import { TechStack } from "../sections/TechStack";
-import Quote from "../sections/Quote";
-import Contact from "../sections/Contact";
+"use client";
 
+import { useRef } from "react";
+import NavBar from "../components/NavBar";
+import Hero from "../components/sections/Hero";
+import ProjectGrid from "../components/sections/ProjectGrid";
+import Quote from "../components/sections/Quote";
+import Contact from "../components/sections/Contact";
 
 export default function Home() {
+  const navbarRef = useRef<HTMLDivElement>(null!);
+
   return (
     <div className="relative">
+      
+  <div
+  ref={navbarRef}
+  className="fixed top-0 left-0 w-full px-6 pointer-events-none"
+  style={{ 
+    zIndex: 50,
+    mixBlendMode: "difference",
+  }}
+>
+  <div className="pointer-events-auto">
+    <NavBar />
+  </div>
+</div>
 
-      {/* <NavBar /> */}
+       <div className="pointer-events-none fixed inset-0 z-[99999] px-6">
 
-      {/* GRID OVERLAY */}
-      <div className="pointer-events-none fixed inset-0 z-[99999] px-6">
         <div className="grid h-full w-full grid-cols-12 gap-6">
+
           {Array.from({ length: 12 }).map((_, i) => (
+
             <div key={i} className="border border-red-500/80 h-full" />
+
           ))}
+
         </div>
+
       </div>
 
-      <Hero/>
+      <Hero navbarRef={navbarRef} />
       <ProjectGrid />
       <Quote />
-      {/* <TechStack /> */}
       <Contact />
     </div>
   );
