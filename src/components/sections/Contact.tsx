b073fe233e88036a1236c115905e0e89d9d219c8
dave <mrdavelopez28@gmail.com>
2026-06-18 02:34:39 +0800

    Update Contact.tsx

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-        <div className="grid grid-cols-12">
+        <div id="contacts" className="grid grid-cols-12">
             <div className="col-span-10 col-start-4 max-h-screen flex flex-col mt-40">
                 <h1 className="uppercase lg:text-5xl font-extrabold">
                     Have an opportunity in mind? <br /> Drop me a message at
@@ -10,7 +10,7 @@ export default function Contact() {
                 <div className="flex gap-48 mt-10">
                     <span className="uppercase lg:text-xs">( GET IN TOUCH )</span>
                     <div className="flex">
-                        <img src="me.png" className="w-25 h-25 rounded-lg" />
+                        <img src="/me.png" className="w-25 h-25 rounded-lg" />
                         <div className="flex flex-col ml-7 h-25 justify-between">
                             <div className="flex flex-col">
                                 <span className="uppercase lg:text-xs font-semibold">LinkedIn</span>
